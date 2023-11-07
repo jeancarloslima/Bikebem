74e93df87cff6e750d33c04f8f722031f5daa206
usuario <jcoliveiralima19@gmail.com>
2023-11-07 11:21:03 -0300

    Tela home criada

--- app/src/main/java/com/example/testeregistro/activity/MainActivity.java
+++ app/src/main/java/com/example/testeregistro/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.testeregistro.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.testeregistro.R;
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mudaTelaRegistrar(View view) {
+        Intent i = new Intent(this, RegistroActivity.class);
+        startActivity(i);
+    }
+
+    public void mudaTelaEntrar(View view) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
