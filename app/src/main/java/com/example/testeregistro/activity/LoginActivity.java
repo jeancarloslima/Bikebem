52ddf7c217cf11c525281caf68b117a416c036b5
sebalter <sebalter612@gmail.com>
2023-11-10 16:45:27 -0300

    Sistema de login finalizado

--- app/src/main/java/com/example/testeregistro/activity/LoginActivity.java
+++ app/src/main/java/com/example/testeregistro/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.testeregistro.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -58,6 +61,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     abrirHome();
+                } else {
+                    String excecao = "";
+                    try{
+                        throw task.getException();
+                    } catch(FirebaseAuthInvalidUserException e) {
+                        excecao = "Usuário não está cadastrado";
+                    } catch(FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "Email ou senha incorreto";
+                    } catch(Exception e) {
+                        excecao = "Erro ao logar " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -65,7 +81,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private void abrirHome() {
         Intent i = new Intent(LoginActivity.this, HomeActivity.class);
-
+        startActivity(i);
     }
 
     private void inicializarComponentes(){
