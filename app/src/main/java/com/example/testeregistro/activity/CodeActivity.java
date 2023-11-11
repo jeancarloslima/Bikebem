586d9b9f6f3f1666ac09b3041b2c2bffdec09ea1
jeancarloslima <144839851+jeancolima@users.noreply.github.com>
2023-11-11 20:05:39 -0300

    Design das telas Home e Bicicleta atualizados

--- app/src/main/java/com/example/testeregistro/activity/CodeActivity.java
+++ app/src/main/java/com/example/testeregistro/activity/CodeActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 
 import com.example.testeregistro.R;
 
-public class BikeActivity extends AppCompatActivity {
+public class CodeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bike);
+        setContentView(R.layout.activity_code);
+    }
+
+    public void abrirBike() {
+        Intent i = new Intent(this, BikeActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/testeregistro/activity/HomeActivity.java
+++ app/src/main/java/com/example/testeregistro/activity/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.testeregistro.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.testeregistro.R;
@@ -12,5 +13,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void abrirCode() {
+        Intent i = new Intent(this, CodeActivity.class);
+        startActivity(i);
     }
 }
